82566e1ad5340f10e8484476fccf5161f3968135
Jonas Steinberg <jonassteinberg1@gmail.com>
2024-06-26 19:45:06 -0500

    ec2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,3 +19,39 @@ resource "aws_route" "nat_gateway_route" {
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.kubernetes_nat_gateway.id
 }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = "vpc-0e76b5906fe7fcc3d"
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = "vpc-0e76b5906fe7fcc3d"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_instance" "ubuntu_server" {
+  ami                    = "ami-00a140d6713ea188d" # Ubuntu Server 22.04 AMI ID, update if necessary
+  instance_type          = "t3a.small"
+  key_name               = "jonas"
+  associate_public_ip_address = true
+  iam_instance_profile   = "admin"
+
+  subnet_id              = "subnet-05005979f45f3dbb7"
+
+  vpc_security_group_ids = ["sg-0153c7640dad0cd3d"]
+
+  tags = {
+    Name = "bastion"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get upgrade -y
+              EOF
+}
+
